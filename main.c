442121d325e090135d8de435501a9691b679986a
brb7nu <brb7nu@virginia.edu>
2015-10-27 09:52:08 -0400

    Write skeleton main function and module for accelerometer

--- main.c
+++ main.c
@@ -1,10 +1,27 @@
-#include <msp430.h> 
+// Will Cray
+// Ben Browning
 
-/*
- * main.c
- */
+#include <msp430.h>
+
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
+    // STARTUP
+    // initialize LED ring
+    // initialize accelerometer
+    // initialize PWM
+
+    // CALIBRATION
+    while () // while device is uncalibrated
+    {
+
+    }
+
+    // NORMAL OPERATION -- LEVEL
+    while (1)
+    {
+
+    }
+
 	return 0;
 }
