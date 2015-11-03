d71260d8cbbd86054aa25cb02c6f90097e391c84
wcc8fs <wcc8fs@virginia.edu>
2015-11-03 10:44:46 -0500

    circular buffer and ADC reading

--- accelerometer.h
+++ accelerometer.h
@@ -2,6 +2,7 @@
 #define ACCELEROMETER_H
 
 #include <msp430.h>
+int n_x = 0;						// index for circular buffer
 
 typedef struct
 {
@@ -14,10 +15,17 @@ typedef struct
 	int xVal;
 	int yVal;
 	int zVal;
+	int xSum;
+	int xAvg;
+	int ySum;
+	int yAvg;
+	int zSum;
+	int zAvg;
 } AccelerometerDefinition;
 
 void initializeAccelerometer(AccelerometerDefinition *accelerometer);
 void calibrateAccelerometer(AccelerometerDefinition *accelerometer);
 void updateAccelerometer(AccelerometerDefinition *accelerometer);
+void filter(AccelerometerDefinition *accelerometer, unsigned int xVal, unsigned int yVal, unsigned int zVal);
 
 #endif
