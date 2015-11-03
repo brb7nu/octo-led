3ac4da0648c318a7e7176b42939576288d78f0cb
brb7nu <brb7nu@virginia.edu>
2015-11-03 14:56:48 -0500

    write pwm function

--- main.c
+++ main.c
@@ -2,12 +2,7 @@
 // Will Cray
 
 #include <msp430.h>
-#include "accelerometer.h"
-#include "LED.h"
 #include "timer.h"
-#include "button.h"
-#include "debounce.h"
-#include "msp430io.h"
 
 // #define LEFT_RED_TP XOUT // P1.0
 // #define LEFT_WHITE_TP YOUT // P1.1
@@ -16,123 +11,51 @@
 // #define RIGHT_WHITE_TP SCLK
 // #define RIGHT_RED_TP XLATCH
 
-typedef enum {initialize, calibrationIndicate, calibrationMeasure, calibrationStore, levelReadADC, levelLighLEDs, levelCORDIC} SystemState;
+// stores the number of milliseconds each led must stay on
+int ledRing[8];
+
+// NO need for LED mask because it just holds the non-zero values in ledRing
+
+void send(char mask)
+{
+	// SPI send the mask to the LED ring
+}
+
+// send a mask based on the LEDs that still have high time remaining
+// run this method at periodic intervals (base of pwm)
+void pwm()
+{
+	char mask = 0x00;
+
+	int i;
+	for (i = 0; i < 8; i++)
+	{
+		if (ledRing[i])
+		{
+			mask |= (0x01 << i);
+			ledRing[i]--;
+		}
+	}
+
+	send(mask);
+}
 
 int main(void) {
 	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
-	SystemState systemState = initialize;
-
 	// initialize timer
 	TimerDefinition timer;
 	initializeTimer(&timer);
-
-	// initialize LED ring
-	LEDRingDefinition ring;
-	initializeLEDRing(&ring);
-
-	// initialize accelerometer
-	AccelerometerDefinition accelerometer;
-	initializeAccelerometer(&accelerometer);
-
-	// initialize button
-	ButtonDefinition pushButton;
-	initializeMSP430IO(&pushButton);
-
-	// initialize PWM
-
-	// Animate LEDs to indicate that initialization state is completed
-	sendLEDMask(N_LED);
-	_delay_cycles(800000);
-	sendLEDMask(NE_LED);
-	_delay_cycles(800000);
-	sendLEDMask(E_LED);
-	_delay_cycles(800000);
-	sendLEDMask(SE_LED);
-	_delay_cycles(800000);
-	sendLEDMask(S_LED);
-	_delay_cycles(800000);
-	sendLEDMask(SW_LED);
-	_delay_cycles(800000);
-	sendLEDMask(W_LED);
-	_delay_cycles(800000);
-	sendLEDMask(NW_LED);
-	_delay_cycles(800000);
-	sendLEDMask(N_LED);
-	_delay_cycles(800000);
-	sendLEDMask(NE_LED);
-	_delay_cycles(800000);
-	sendLEDMask(E_LED);
-	_delay_cycles(800000);
-	sendLEDMask(SE_LED);
-	_delay_cycles(800000);
-	sendLEDMask(S_LED);
-	_delay_cycles(800000);
-	sendLEDMask(SW_LED);
-	_delay_cycles(800000);
-	sendLEDMask(W_LED);
-	_delay_cycles(800000);
-	sendLEDMask(NW_LED);
-	_delay_cycles(800000);
-	sendLEDMask(N_LED);
-	_delay_cycles(800000);
-	sendLEDMask(NE_LED | N_LED);
-	_delay_cycles(800000);
-	sendLEDMask(E_LED | NE_LED | N_LED);
-	_delay_cycles(800000);
-	sendLEDMask(SE_LED | E_LED | NE_LED | N_LED);
-	_delay_cycles(800000);
-	sendLEDMask(S_LED | SE_LED | E_LED | NE_LED | N_LED);
-	_delay_cycles(800000);
-	sendLEDMask(SW_LED | S_LED | SE_LED | E_LED | NE_LED | N_LED);
-	_delay_cycles(800000);
-	sendLEDMask(W_LED | SW_LED | S_LED | SE_LED | E_LED | NE_LED | N_LED);
-	_delay_cycles(800000);
-	sendLEDMask(NW_LED | W_LED | SW_LED | S_LED | SE_LED | E_LED | NE_LED | N_LED);
-	_delay_cycles(800000);
 
 	_BIS_SR(GIE);
 
 	while (1)
 	{
 		updateTimer(&timer);
-		updateButtonState(&pushButton, &timer);
-		// updateAccelerometer(&accelerometer);
-		updateLEDRing(&ring, &timer);
 
-		switch (systemState)
+		if (timer.milliseconds > 15)
 		{
-			case initialize:
-				systemState = calibrationIndicate;
-				break;
-			case calibrationIndicate:
-				// flash all LEDs to tell user to calibrate
-
-
-				// exit conditions for this state
-				if (pushButton.state == pressed)
-				{
-					// calibrate this axis
-					systemState = calibrationMeasure;
-				}
-				break;
-			case calibrationMeasure:
-				// read the 8-sample buffer for x, y, or z
-				P1OUT |= BIT6;
-				_delay_cycles(6400000);
-				P1OUT &= ~BIT6;
-				systemState = calibrationIndicate;
-				break;
-			case calibrationStore:
-				break;
-			case levelReadADC:
-				break;
-			case levelLighLEDs:
-				break;
-			case levelCORDIC:
-				break;
-			default:
-				break;
+			pwm();
 		}
 	}
 
