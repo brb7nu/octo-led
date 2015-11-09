9e4e2da8f81cdd9e7ae33f8712e8fbda6c4630a5
brb7nu <brb7nu@virginia.edu>
2015-11-08 21:35:42 -0500

    describe accelerometer struct variables

--- accelerometer.h
+++ accelerometer.h
@@ -7,22 +7,24 @@ extern volatile unsigned int buffer[3][8];
 
 typedef struct
 {
-	int xOffset;
+	int xOffset; // scaled value from -100 to 100
+	int xMax; // max value, set by calibration
+	int xMin; // min value, set by calibration
+	int xVal; // most recent unscaled reading
+	int xSum; // the sum of the current and past 7 readings
+	int xAvg; // sum / 8, which is the same as sum >> 3
+
 	int yOffset;
-	int zOffset;
-	int xMax;
-	int xMin;
 	int yMax;
 	int yMin;
-	int zMax;
-	int zMin;
-	int xVal;
 	int yVal;
-	int zVal;
-	int xSum;
-	int xAvg;
 	int ySum;
 	int yAvg;
+
+	int zOffset;
+	int zMax;
+	int zMin;
+	int zVal;
 	int zSum;
 	int zAvg;
 } AccelerometerDefinition;
